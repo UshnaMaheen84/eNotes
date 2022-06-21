547f36b75b415c4fc8c632c0fa8bda9feaefc2e1
ghousmuhammadkhan786@gmail.com <hassan.khan@ezyapps.net>
2022-06-22 01:41:57 +0500

    fix listing not refreshing when adding new notes, added kotlin extension to avoid calling findviewbyid for every views

--- app/src/main/java/com/example/enotes/adapter/AdapterFile_attach.kt
+++ app/src/main/java/com/example/enotes/adapter/AdapterFile_attach.kt
@@ -3,35 +3,38 @@ package com.example.enotes.adapter
 import android.content.Context
 import android.net.Uri
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.enotes.R
 
-class AdapterFile_attach(val file_uri : List<Uri> ,val file_name: ArrayList<String>, context: Context) : RecyclerView.Adapter<AdapterFile_attach.ViewHolder>(){
+class AdapterFile_attach(
+    val file_uri: List<Uri>,
+    val file_name: ArrayList<String>,
+    var context: Context
+) : RecyclerView.Adapter<AdapterFile_attach.ViewHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.file_attach,parent,false);
+        var itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.file_attach, parent, false)
         return ViewHolder(itemView)
 
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val uri: Uri = file_uri.get(position)
-        val name:String= file_name.get(position)
+        val name: String = file_name.get(position)
         holder.image.setImageURI(uri)
         holder.tv.setText(name)
-        Log.e("img",uri.toString())
+        Log.e("img", uri.toString())
     }
 
-    override fun getItemCount()= file_uri.size
+    override fun getItemCount() = file_uri.size
 
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         val image: ImageView = itemView.findViewById(R.id.attach_file)
         val tv: TextView = itemView.findViewById(R.id.file_name2)
